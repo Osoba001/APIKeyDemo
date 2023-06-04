39a93c71d39073d7fdaeb485131f3d029936043c
Kelly E. Osoba <89794417+Osoba001@users.noreply.github.com>
2023-06-04 09:00:32 +0100

    Api key filter for returning custom result
    this does not appear to be a good practice

---
+++ APIKeyDemo/ApiKeyEndpointFilterCustomObjectResult.cs
@@ -0,0 +1,28 @@
+﻿namespace APIKeyDemo
+{
+    public class ApiKeyEndpointFilterCustomObjectResult: IEndpointFilter
+    {
+        private readonly IConfiguration _config;
+
+        public ApiKeyEndpointFilterCustomObjectResult(IConfiguration config)
+        {
+            _config = config;
+        }
+        public async ValueTask<object?> InvokeAsync(
+            EndpointFilterInvocationContext context,
+            EndpointFilterDelegate next)
+        {
+            if (!context.HttpContext.Request.Headers.TryGetValue(ApiKeyHeaderName, out var extractedApiKey))
+            {
+                return new UnauthorizedHttpObjectResult("Api Key is Missing.");
+            }
+            var apikey = _config.GetValue<string>(ApiKeySectionName);
+            if (!apikey.Equals(extractedApiKey))
+            {
+                return new UnauthorizedHttpObjectResult("Api Key is Missing.");
+            }
+            return await next(context);
+        }
+    }
+
+}
---
+++ APIKeyDemo/UnauthorizedHttpObjectResult.cs
@@ -0,0 +1,27 @@
+﻿namespace APIKeyDemo
+{
+    public sealed class UnauthorizedHttpObjectResult:IResult,IStatusCodeHttpResult
+    {
+        private readonly object _body;
+
+        public UnauthorizedHttpObjectResult(object body)
+        {
+            _body = body;
+        }
+        public int StatusCode => StatusCodes.Status401Unauthorized;
+        int? IStatusCodeHttpResult.StatusCode => StatusCode;
+        public async Task ExecuteAsync(HttpContext httpContext)
+        {
+            ArgumentNullException.ThrowIfNull(httpContext);
+
+            httpContext.Response.StatusCode = StatusCode;
+            if(_body is string s)
+            {
+                await httpContext.Response.WriteAsync(s);
+            }
+
+            await httpContext.Response.WriteAsJsonAsync(_body);
+        }
+    }
+
+}
