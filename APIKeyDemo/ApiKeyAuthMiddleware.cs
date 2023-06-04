8b57793996c39756d9755867be9ad5adf0ee4fdf
Kelly E. Osoba <89794417+Osoba001@users.noreply.github.com>
2023-06-04 08:57:35 +0100

    Middleware based api key
    Created middleware for managing api key

---
+++ APIKeyDemo/ApiKeyAuthMiddleware.cs
@@ -0,0 +1,31 @@
+﻿global using static APIKeyDemo.AuthConstants;
+namespace APIKeyDemo
+{
+    public class ApiKeyAuthMiddleware : IMiddleware
+    {
+        private readonly IConfiguration _config;
+
+        public ApiKeyAuthMiddleware(IConfiguration config)
+        {
+            _config = config;
+        }
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            if (!context.Request.Headers.TryGetValue(ApiKeyHeaderName, out var extractedApiKey))
+            {
+                //ontext.Response.ContentType = "ap";
+                context.Response.StatusCode = 401;
+                await context.Response.WriteAsync("API Key missing");
+                return;
+            }
+            var apikey = _config.GetValue<string>(ApiKeySectionName);
+            if (!apikey.Equals(extractedApiKey))
+            {
+                context.Response.StatusCode = 401;
+                await context.Response.WriteAsync("API Key Invalid");
+                return;
+            }
+            await next(context);
+        }
+    }
+}
