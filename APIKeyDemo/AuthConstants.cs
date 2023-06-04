ebf5500e7ebd96f82122b13d122640cfc6723c27
Kelly E. Osoba <89794417+Osoba001@users.noreply.github.com>
2023-06-04 08:58:24 +0100

    A file to holder constant values

---
+++ APIKeyDemo/AuthConstants.cs
@@ -0,0 +1,9 @@
+﻿namespace APIKeyDemo
+{
+    public static class AuthConstants
+    {
+        public const string ApiKeyHeaderName = "x-api-key";
+        public const string ApiKeySectionName = "Authentication:ApiKey";
+
+    }
+}
