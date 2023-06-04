3153beb7b0726cadcc3a399a67b4ede245ff815d
Kelly E. Osoba <89794417+Osoba001@users.noreply.github.com>
2023-06-04 08:53:49 +0100

    Api key authentication filter

---
+++ APIKeyDemo/ApiKeyEndpointFilter.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace APIKeyDemo
+{
+    public class ApiKeyEndpointFilter : IEndpointFilter
+    {
+        private readonly IConfiguration _config;
+
+        public ApiKeyEndpointFilter(IConfiguration config)
+        {
+            _config = config;
+        }
+        public async ValueTask<object?> InvokeAsync(
+            EndpointFilterInvocationContext context, 
+            EndpointFilterDelegate next)
+        {
+            if (!context.HttpContext.Request.Headers.TryGetValue(ApiKeyHeaderName, out var extractedApiKey))
+            {
+               return TypedResults.Unauthorized();
+            }
+            var apikey = _config.GetValue<string>(ApiKeySectionName);
+            if (!apikey.Equals(extractedApiKey))
+            {
+                return TypedResults.Unauthorized();
+            }
+            return await next(context);
+        }
+    }
+}
