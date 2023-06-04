497aefaed8a3e9410952fba77c224c1ba4207bc0
Kelly E. Osoba <89794417+Osoba001@users.noreply.github.com>
2023-06-04 08:52:56 +0100

    Created api attribute for service filter

---
+++ APIKeyDemo/ApiKeyAttribute.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace APIKeyDemo
+{
+    public class ApiKeyAttribute:ServiceFilterAttribute
+    {
+        public ApiKeyAttribute():base(typeof(ApiKeyAuthFilter))
+        {
+
+        }
+    }
+}
