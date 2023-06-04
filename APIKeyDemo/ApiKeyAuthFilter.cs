8c0cef47180f63ea57af26508f22b98dfa634136
Kelly E. Osoba <89794417+Osoba001@users.noreply.github.com>
2023-06-04 08:55:52 +0100

    Created api key authentication filter

---
+++ APIKeyDemo/ApiKeyAuthFilter.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace APIKeyDemo
+{
+    public class ApiKeyAuthFilter : IAuthorizationFilter
+    {
+        private readonly IConfiguration _config;
+
+        public ApiKeyAuthFilter(IConfiguration config)
+        {
+            _config = config;
+        }
+        public void OnAuthorization(AuthorizationFilterContext context)
+        {
+            if (!context.HttpContext.Request.Headers.TryGetValue(ApiKeyHeaderName, out var extractedApiKey))
+            {
+                //ontext.Response.ContentType = "ap";
+                context.Result = new UnauthorizedObjectResult("Api Key is Missing.");
+                return;
+            }
+            var apikey = _config.GetValue<string>(ApiKeySectionName);
+            if (!apikey.Equals(extractedApiKey))
+            {
+                context.Result = new UnauthorizedObjectResult("Invalid Api Key.");
+                return;
+            }
+        }
+    }
+}
